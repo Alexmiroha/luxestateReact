cc6d2f3a05d2ac0f80f8537b25b8d8f0b25d2485
Alexmiroha <Alexmiroha@mail.ru>
2022-01-30 23:12:10 +0100

    some change in Apartments Component and styles for that

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -3,7 +3,7 @@ import {Header} from "./HeaderComponent/Header";
 import {Main} from "./MainComponent/Main";
 import {Footer} from "./FooterComponent/Footer";
 import {ScrollButton} from "./GlobalReusableComponents/ScrollButton";
-import {animateScroll as scroll} from "react-scroll";
+
 
 export const App = () => {
     return (
--- src/App/MainComponent/ApartmentsComponent/Apartments.jsx
+++ src/App/MainComponent/ApartmentsComponent/Apartments.jsx
@@ -1,26 +1,50 @@
 import React from 'react'
+import s from './Apartments.module.css'
 
 export const Apartments = () => {
     return (
-        <section className="rent" id="Apartments">
-            <div className="wrapper apart">
+        <section className={s.rent} id="Apartments">
+            <div className={`wrapper ${s.apart}`}>
                 <h2>More Then 500+ <br/> Apartments For Rent</h2>
-                <div className="cards">
-                    <div className="card">
+                <div className={s.cards}>
+                    <div className={s.card}>
                         <img src="./src/img/apart.jpeg" alt=""/>
-                        <div className="card_info">
+                        <div className={s.card_info}>
                             <p>Random Street, Apt B453, New York</p>
-                            <div className="priceitp">
-                                <div className="price">$3,500</div>
-                                <div className="info_card">
+                            <div className={s.priceitp}>
+                                <div className={s.price}>$3,500</div>
+                                <div className={s.info_card}>
                                 <span>
-                                    <svg className="icon info_card_icon" width="18" height="18">
+                                    <svg className={`${s.icon} ${s.info_card_icon}`} width="18" height="18">
+                                        <use href="./src/img/sprite.svg#shower-solid"/>
+                                    </svg>
+                                    2BA
+                                </span>
+                                    <span>
+                                    <svg className={`${s.icon} ${s.info_card_icon}`} width="18" height="18">
+                                        <use href="./src/img/sprite.svg#bed-solid"/>
+                                    </svg>
+                                    2BD
+                                </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={s.card}>
+                        <img src="./src/img/apart.jpeg" alt=""/>
+                        <div className={s.card_info}>
+                            <p>Random Street, Apt B453, New York</p>
+                            <div className={s.priceitp}>
+                                <div className={s.price}>$3,500</div>
+                                <div className={s.info_card}>
+                                <span>
+                                    <svg className={`${s.icon} ${s.info_card_icon}`} width="18" height="18">
                                         <use href="./src/img/sprite.svg#shower-solid"></use>
                                     </svg>
                                     2BA
                                 </span>
                                     <span>
-                                    <svg className="icon info_card_icon" width="18" height="18">
+                                    <svg className={`${s.icon} ${s.info_card_icon}`} width="18" height="18">
                                         <use href="./src/img/sprite.svg#bed-solid"></use>
                                     </svg>
                                     2BD
@@ -29,21 +53,21 @@ export const Apartments = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="card">
+                    <div className={s.card}>
                         <img src="./src/img/apart.jpeg" alt=""/>
-                        <div className="card_info">
+                        <div className={s.card_info}>
                             <p>Random Street, Apt B453, New York</p>
-                            <div className="priceitp">
-                                <div className="price">$3,500</div>
-                                <div className="info_card">
+                            <div className={s.priceitp}>
+                                <div className={s.price}>$3,500</div>
+                                <div className={s.info_card}>
                                 <span>
-                                    <svg className="icon info_card_icon" width="18" height="18">
+                                    <svg className={`${s.icon} ${s.info_card_icon}`} width="18" height="18">
                                         <use href="./src/img/sprite.svg#shower-solid"></use>
                                     </svg>
                                     2BA
                                 </span>
                                     <span>
-                                    <svg className="icon info_card_icon" width="18" height="18">
+                                    <svg className={`${s.icon} ${s.info_card_icon}`} width="18" height="18">
                                         <use href="./src/img/sprite.svg#bed-solid"></use>
                                     </svg>
                                     2BD
@@ -52,21 +76,21 @@ export const Apartments = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="card">
+                    <div className={s.card}>
                         <img src="./src/img/apart.jpeg" alt=""/>
-                        <div className="card_info">
+                        <div className={s.card_info}>
                             <p>Random Street, Apt B453, New York</p>
-                            <div className="priceitp">
-                                <div className="price">$3,500</div>
-                                <div className="info_card">
+                            <div className={s.priceitp}>
+                                <div className={s.price}>$3,500</div>
+                                <div className={s.info_card}>
                                 <span>
-                                    <svg className="icon info_card_icon" width="18" height="18">
+                                    <svg className={`${s.icon} ${s.info_card_icon}`} width="18" height="18">
                                         <use href="./src/img/sprite.svg#shower-solid"></use>
                                     </svg>
                                     2BA
                                 </span>
                                     <span>
-                                    <svg className="icon info_card_icon" width="18" height="18">
+                                    <svg className={`${s.icon} ${s.info_card_icon}`} width="18" height="18">
                                         <use href="./src/img/sprite.svg#bed-solid"></use>
                                     </svg>
                                     2BD
@@ -75,21 +99,21 @@ export const Apartments = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="card">
+                    <div className={s.card}>
                         <img src="./src/img/apart.jpeg" alt=""/>
-                        <div className="card_info">
+                        <div className={s.card_info}>
                             <p>Random Street, Apt B453, New York</p>
-                            <div className="priceitp">
-                                <div className="price">$3,500</div>
-                                <div className="info_card">
+                            <div className={s.priceitp}>
+                                <div className={s.price}>$3,500</div>
+                                <div className={s.info_card}>
                                 <span>
-                                    <svg className="icon info_card_icon" width="18" height="18">
+                                    <svg className={`${s.icon} ${s.info_card_icon}`} width="18" height="18">
                                         <use href="./src/img/sprite.svg#shower-solid"></use>
                                     </svg>
                                     2BA
                                 </span>
                                     <span>
-                                    <svg className="icon info_card_icon" width="18" height="18">
+                                    <svg className={`${s.icon} ${s.info_card_icon}`} width="18" height="18">
                                         <use href="./src/img/sprite.svg#bed-solid"></use>
                                     </svg>
                                     2BD
@@ -98,44 +122,21 @@ export const Apartments = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="card">
+                    <div className={s.card}>
                         <img src="./src/img/apart.jpeg" alt=""/>
-                        <div className="card_info">
+                        <div className={s.card_info}>
                             <p>Random Street, Apt B453, New York</p>
-                            <div className="priceitp">
-                                <div className="price">$3,500</div>
-                                <div className="info_card">
+                            <div className={s.priceitp}>
+                                <div className={s.price}>$3,500</div>
+                                <div className={s.info_card}>
                                 <span>
-                                    <svg className="icon info_card_icon" width="18" height="18">
+                                    <svg className={`${s.icon} ${s.info_card_icon}`} width="18" height="18">
                                         <use href="./src/img/sprite.svg#shower-solid"></use>
                                     </svg>
                                     2BA
                                 </span>
                                     <span>
-                                    <svg className="icon info_card_icon" width="18" height="18">
-                                        <use href="./src/img/sprite.svg#bed-solid"></use>
-                                    </svg>
-                                    2BD
-                                </span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="card">
-                        <img src="./src/img/apart.jpeg" alt=""/>
-                        <div className="card_info">
-                            <p>Random Street, Apt B453, New York</p>
-                            <div className="priceitp">
-                                <div className="price">$3,500</div>
-                                <div className="info_card">
-                                <span>
-                                    <svg className="icon info_card_icon" width="18" height="18">
-                                        <use href="./src/img/sprite.svg#shower-solid"></use>
-                                    </svg>
-                                    2BA
-                                </span>
-                                    <span>
-                                    <svg className="icon info_card_icon" width="18" height="18">
+                                    <svg className={`${s.icon} ${s.info_card_icon}`} width="18" height="18">
                                         <use href="./src/img/sprite.svg#bed-solid"></use>
                                     </svg>
                                     2BD
@@ -145,14 +146,14 @@ export const Apartments = () => {
                         </div>
                     </div>
                 </div>
-                <div className="search-button">
+                <div className={s.search_button}>
                     <form action="">
                         <label>
                             <input type="text" placeholder="Search Locations"/>
                         </label>
                         <input type="submit" value="search"/>
                     </form>
-                    <button className="all_ap">View All Apartments</button>
+                    <button className={s.all_ap}>View All Apartments</button>
                 </div>
             </div>
         </section>
